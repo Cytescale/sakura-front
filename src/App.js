60d881ee09310dc9e9bc759c297aaebcdb3828ba
cytescale <nikhil@cytescale.com>
2021-01-16 22:19:49 +0530

    dev commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+/* eslint-disable no-useless-constructor */
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Header extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(<>
+      <div className='app_header_main_cont'> 
+        Header
+      </div>
+    </>)
+  }
 }
 
-export default App;
+
+export default class App extends React.Component{
+
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(<>
+      <div>
+        <Header/>
+      </div>
+    </>)
+  }
+}
